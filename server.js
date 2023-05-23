9a4cc173c4a8ef2d6ffb916a6d02d7630bd8b517
AWildLlamaa <63163194+AWildLlamaa@users.noreply.github.com>
2023-05-23 12:28:21 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,49 +1,32 @@
 const express = require('express');
-const OAuth = require('oauth-1.0a');
-const crypto = require('crypto');
-const fetch = require('node-fetch');
+const Twit = require('twit');
 
 const app = express();
 const port = 3000;
 
+const apiKey = 'i4wpQCijxC2f8pl2tY0O0ERAK';
+const apiSecret = 'IBkYzXlRgKyOrVp0php1NhwR8GHCMpMtOs3TZtXdE3zpuXveE6';
+const accessToken = '1586804888289296389-VWw6t438vq3QjzQgtAbbgaIS1MHBnO';
+const accessTokenSecret = '4gfe29e8hSf1r2PjqKeWwL2dTaDU7xwQrUFIPEFVUZ6Zv';
+
+const twitClient = new Twit({
+  consumer_key: apiKey,
+  consumer_secret: apiSecret,
+  access_token: accessToken,
+  access_token_secret: accessTokenSecret,
+});
+
 // Endpoint to handle the Twitter API request
 app.get('/likes/:username', async (req, res) => {
   const username = req.params.username;
-  const apiKey = 'i4wpQCijxC2f8pl2tY0O0ERAK';
-  const apiSecret = 'IBkYzXlRgKyOrVp0php1NhwR8GHCMpMtOs3TZtXdE3zpuXveE6';
-  const accessToken = '1586804888289296389-VWw6t438vq3QjzQgtAbbgaIS1MHBnO';
-  const accessTokenSecret = '4gfe29e8hSf1r2PjqKeWwL2dTaDU7xwQrUFIPEFVUZ6Zv';
-
-  // Set up OAuth1.0a authentication
-  const oauth = OAuth({
-    consumer: { key: apiKey, secret: apiSecret },
-    signature_method: 'HMAC-SHA1',
-    hash_function: (baseString, key) => crypto.createHmac('sha1', key).update(baseString).digest('base64')
-  });
-
-  const requestData = {
-    url: `https://api.twitter.com/1.1/favorites/list.json?screen_name=${username}`,
-    method: 'GET'
-  };
-
-  const token = {
-    key: accessToken,
-    secret: accessTokenSecret
-  };
-
-  const requestOptions = {
-    headers: oauth.toHeader(oauth.authorize(requestData, token))
-  };
 
   try {
     // Make a request to fetch the likes using the Twitter API
-    const response = await fetch(requestData.url, requestOptions);
+    const response = await twitClient.get('favorites/list', {
+      screen_name: username,
+    });
 
-    if (!response.ok) {
-      throw new Error('Failed to fetch likes');
-    }
-
-    const data = await response.json();
+    const data = response.data;
 
     // Extract tweet ID and media URL from each liked tweet
     const likes = data.map(tweet => {
