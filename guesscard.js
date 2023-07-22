1aab27a7a9eada0d07968533863c727ed9942623
AWildLlamaa <63163194+AWildLlamaa@users.noreply.github.com>
2023-07-22 18:55:08 -0400

    Add files via upload

--- guesscard.js
+++ guesscard.js
@@ -21,10 +21,38 @@ function displayCard(data) {
 
 // Function to ask a question to ChatGPT API
 async function askQuestion(question) {
-  const chatGptApiKey = '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';
-
-  // Rest of the askQuestion function remains the same
-}
+    // Replace 'YOUR_CHATGPT_API_KEY' with your actual ChatGPT API key
+    const chatGptApiKey = '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';
+  
+    try {
+      const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${chatGptApiKey}`,
+        },
+        body: JSON.stringify({
+          'prompt': `You are playing a game of 20 Questions to guess a Magic card. Is the card ${question}?`,
+          'temperature': 0.7,
+          'max_tokens': 100,
+          'stop': '\n',
+        }),
+      });
+  
+      // Check if the response status is 404 (Not Found)
+      if (response.status === 404) {
+        throw new Error('API endpoint not found. Please check the API URL.');
+      }
+  
+      const data = await response.json();
+      const answer = data.choices[0]?.text.trim(); // Use optional chaining to handle possible undefined response
+      return answer || "I'm sorry, I couldn't answer that question.";
+    } catch (error) {
+      console.error('Error fetching response from ChatGPT API:', error);
+      return "I'm sorry, I couldn't answer that question.";
+    }
+  }
+  
 
 // Function to handle user input and game flow
 async function handleQuestion() {
