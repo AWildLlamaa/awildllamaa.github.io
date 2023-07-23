46a2823d41b83d46f7a1e88a5e091c25a6ee5484
AWildLlamaa <63163194+AWildLlamaa@users.noreply.github.com>
2023-07-23 18:10:00 -0400

    Add files via upload

--- netlify/functions/askChatGPT.js
+++ netlify/functions/askChatGPT.js
@@ -43,38 +43,44 @@ exports.handler = async function(event, context) {
   }, null, 2));
 
   try {
-    const response = await fetch(`https://api.openai.com/v1/engines/davinci/completions`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${apiKey}`,
-      },
-      body: JSON.stringify({
-        prompt: question,
-        max_tokens: 50,
-        temperature: 0.5,
-        stop_sequences: ["Yes", "No", "I don't know"]
-      }),
-    });
+        const response = await fetch(`https://api.openai.com/v1/engines/davinci/completions`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${apiKey}`,
+            },
+            body: JSON.stringify({
+                prompt: question,
+                max_tokens: 50,
+                temperature: 0.5,
+                stop_sequences: ["Yes", "No", "I don't know"]
+            }),
+        });
 
-    if (!response.ok) {
-      console.error('OpenAI API error response:', await response.text());
-      throw new Error(`OpenAI API call failed with status: ${response.status}`);
+        // Check and log if the response is not OK
+        if (!response.ok) {
+            const errorResponse = await response.text();
+            console.error('OpenAI API Response:', errorResponse); // Log the detailed error response from OpenAI
+            throw new Error(`OpenAI API call failed with status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        const answer = data.choices[0].text.trim();
+        console.log('OpenAI API Payload:', JSON.stringify({
+            prompt: question,
+            max_tokens: 50,
+            temperature: 0.5,
+            stop_sequences: ["Yes", "No", "I don't know"]
+        })); // Log the request payload
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ answer: answer }),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: `Error processing the request: ${error.message}` }),
+        };
     }
-
-    const data = await response.json();
-    const answer = data.choices[0].text.trim();
-    console.log(data);
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ answer: answer }),
-    };
-  } catch (error) {
-    console.error('Error in lambda function:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({error: `Error processing the request: ${error.message}`}),
-    };
-  }
 };
