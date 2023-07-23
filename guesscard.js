4c8a515ad19743d98192449a14ca32ca7f546686
AWildLlamaa <63163194+AWildLlamaa@users.noreply.github.com>
2023-07-23 17:07:22 -0400

    Add files via upload

--- guesscard.js
+++ guesscard.js
@@ -25,50 +25,47 @@ async function askQuestion(question) {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          question: `Given the information about the Magic the Gathering card "${cardData.name}" with attributes like color identity and type, answer this question about the card with either "Yes", "No", or "I didn't quite understand that. Please ask again.": ${question}?`,
+          question: `Given the information about the Magic the Gathering card "${cardData.name}" with attributes like color identity "${cardData.color_identity.join(', ')}" and type "${cardData.type_line}", answer this question about the card with either "Yes", "No", or "I didn't quite understand that. Please ask again.": ${question}?`,
         }),
       });
-      
-      const responseBody = await response.text();
-      
+  
       if (!response.ok) {
-        try {
-          const errorData = JSON.parse(responseBody);
-          console.error('Error from server:', errorData);
-        } catch (jsonError) {
-          console.error('Error from server (non-JSON response):', responseBody);
-        }
-        throw new Error('Server error');
+        const errorData = await response.text();
+        throw new Error(`Error from server: ${errorData}`);
       }
   
-      const data = JSON.parse(responseBody);
+      const data = await response.json();
       return data.answer;
     } catch (error) {
       console.error('Error fetching response from ChatGPT API:', error);
       throw error;
     }
-}
-
-async function handleQuestion() {
-  const userInput = document.getElementById('userInput').value.trim();
-  if (!userInput) return;
-
-  document.getElementById('userInput').value = '';
-  attempts++;
-
-  console.log(`Asking question: ${userInput}`);
-  const response = await askQuestion(userInput);
-  console.log(`Received response: ${response}`);
-
-  const responseDiv = document.getElementById('response');
-  responseDiv.innerHTML = `Response: ${response}`;
-
-  if (response === "Yes") {
-    responseDiv.innerHTML += `<br>Correct! You guessed the card "${cardData.name}" in ${attempts} attempts.`;
-    attempts = 0;
-    getRandomCard();
   }
-}
+  
+  async function handleQuestion() {
+    const userInput = document.getElementById('userInput').value.trim();
+    if (!userInput) return;
+  
+    document.getElementById('userInput').value = '';
+    attempts++;
+  
+    console.log(`Asking question: ${userInput}`);
+    try {
+      const response = await askQuestion(userInput);
+      console.log(`Received response: ${response}`);
+  
+      const responseDiv = document.getElementById('response');
+      responseDiv.innerHTML = `Response: ${response}`;
+  
+      if (response === "Yes" || response === "No") {
+        responseDiv.innerHTML += `<br>Correct! You guessed the card "${cardData.name}" in ${attempts} attempts.`;
+        attempts = 0;
+        getRandomCard();
+      }
+    } catch (error) {
+      console.error('Error handling question:', error);
+    }
+  }
 
 document.getElementById('askButton').addEventListener('click', handleQuestion);
 getRandomCard();
