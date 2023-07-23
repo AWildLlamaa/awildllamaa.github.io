3c725daacfed4a8de978443b8fb2040ecafaae61
AWildLlamaa <63163194+AWildLlamaa@users.noreply.github.com>
2023-07-23 14:50:16 -0400

    Add files via upload

---
+++ netlify/functions/askChatGPT.js
@@ -0,0 +1,41 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function (event, context) {
+    if (event.httpMethod !== 'POST') {
+        return {
+            statusCode: 405,
+            body: 'Method Not Allowed',
+        };
+    }
+
+    const payload = JSON.parse(event.body);
+    const question = payload.question;
+
+    const apiKey = process.env.CHATGPT_API_KEY;
+
+    try {
+        const response = await fetch(`https://api.openai.com/v1/engines/davinci-codex/completions`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${apiKey}`,
+            },
+            body: JSON.stringify({
+                prompt: question,
+                max_tokens: 150,
+            }),
+        });
+
+        const data = await response.json();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ answer: data.choices[0].text.trim() }),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: 'Error processing the request.',
+        };
+    }
+};
