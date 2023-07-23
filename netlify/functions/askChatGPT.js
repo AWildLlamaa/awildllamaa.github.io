a18276d044236eaa197f95a045d5aba76a4f88f3
AWildLlamaa <63163194+AWildLlamaa@users.noreply.github.com>
2023-07-23 18:27:50 -0400

    Add files via upload

--- netlify/functions/askChatGPT.js
+++ netlify/functions/askChatGPT.js
@@ -1,48 +1,41 @@
 let fetch;
 if (typeof window === 'undefined') {
-  fetch = require('node-fetch');
+    fetch = require('node-fetch');
 } else {
-  import('node-fetch').then(module => {
-    fetch = module.default;
-  });
+    import('node-fetch').then(module => {
+        fetch = module.default;
+    });
 }
 
 exports.handler = async function(event, context) {
-  if (event.httpMethod !== 'POST') {
-    return {
-      statusCode: 405,
-      body: JSON.stringify({error: 'Method Not Allowed'}),
-    };
-  }
+    if (event.httpMethod !== 'POST') {
+        return {
+            statusCode: 405,
+            body: 'Method Not Allowed',
+        };
+    }
 
-  let payload;
-  try {
-    payload = JSON.parse(event.body);
-  } catch (e) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({error: 'Invalid JSON payload received.'}),
-    };
-  }
+    let payload;
+    try {
+        payload = JSON.parse(event.body);
+    } catch (e) {
+        return {
+            statusCode: 400,
+            body: 'Invalid JSON payload received.',
+        };
+    }
 
-  if (!payload || !payload.question) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({error: 'Payload must contain a "question" property.'}),
-    };
-  }
+    if (!payload || !payload.question) {
+        return {
+            statusCode: 400,
+            body: 'Payload must contain a "question" property.',
+        };
+    }
 
-  const question = payload.question;
-  const apiKey = process.env.CHATGPT_API_KEY;
+    const question = payload.question;
+    const apiKey = process.env.CHATGPT_API_KEY;
 
-  console.log('Sending to OpenAI:', JSON.stringify({
-    prompt: question,
-    max_tokens: 50,
-    temperature: 0.5,
-    stop_sequences: ["Yes", "No", "I don't know"]
-  }, null, 2));
-
-  try {
+    try {
         const response = await fetch(`https://api.openai.com/v1/engines/davinci/completions`, {
             method: "POST",
             headers: {
@@ -51,26 +44,15 @@ exports.handler = async function(event, context) {
             },
             body: JSON.stringify({
                 prompt: question,
-                max_tokens: 20,
-                temperature: 0.3
+                max_tokens: 50,
+                temperature: 0.5
+                // Removed stop_sequences for testing
             }),
         });
 
-        // Check and log if the response is not OK
-        if (!response.ok) {
-            const errorResponse = await response.text();
-            console.error('OpenAI API Response:', errorResponse); // Log the detailed error response from OpenAI
-            throw new Error(`OpenAI API call failed with status: ${response.status}`);
-        }
-
         const data = await response.json();
         const answer = data.choices[0].text.trim();
-        console.log('OpenAI API Payload:', JSON.stringify({
-            prompt: question,
-            max_tokens: 50,
-            temperature: 0.5,
-            stop_sequences: ["Yes", "No", "I don't know"]
-        })); // Log the request payload
+        console.log(data);  // Logging the response for debugging
 
         return {
             statusCode: 200,
