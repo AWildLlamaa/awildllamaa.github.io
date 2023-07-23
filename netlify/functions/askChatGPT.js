952267cedaf094f9af382171e04d3936cef36b2b
AWildLlamaa <63163194+AWildLlamaa@users.noreply.github.com>
2023-07-23 17:59:00 -0400

    Add files via upload

--- netlify/functions/askChatGPT.js
+++ netlify/functions/askChatGPT.js
@@ -1,75 +1,79 @@
 let fetch;
 if (typeof window === 'undefined') {
-    fetch = require('node-fetch');
+  fetch = require('node-fetch');
 } else {
-    import('node-fetch').then(module => {
-        fetch = module.default;
-    });
+  import('node-fetch').then(module => {
+    fetch = module.default;
+  });
 }
 
 exports.handler = async function(event, context) {
-    if (event.httpMethod !== 'POST') {
-        return {
-            statusCode: 405,
-            body: JSON.stringify({ message: 'Method Not Allowed' }),
-        };
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: 'Method Not Allowed',
+    };
+  }
+
+  let payload;
+  try {
+    payload = JSON.parse(event.body);
+  } catch (e) {
+    return {
+      statusCode: 400,
+      body: 'Invalid JSON payload received.',
+    };
+  }
+
+  if (!payload || !payload.question) {
+    return {
+      statusCode: 400,
+      body: 'Payload must contain a "question" property.',
+    };
+  }
+
+  const question = payload.question;
+  const apiKey = process.env.CHATGPT_API_KEY;
+
+  console.log('Sending to OpenAI:', JSON.stringify({
+    prompt: question,
+    max_tokens: 50,
+    temperature: 0.5,
+    stop_sequences: ["Yes", "No", "I don't know"]
+  }, null, 2));
+
+  try {
+    const response = await fetch(`https://api.openai.com/v1/engines/davinci/completions`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify({
+        prompt: question,
+        max_tokens: 50,
+        temperature: 0.5,
+        stop_sequences: ["Yes", "No", "I don't know"]
+      }),
+    });
+
+    if (!response.ok) {
+      console.error('OpenAI API error response:', await response.text());
+      throw new Error(`OpenAI API call failed with status: ${response.status}`);
     }
 
-    let payload;
-    try {
-        payload = JSON.parse(event.body);
-    } catch (e) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({ message: 'Invalid JSON payload received.' }),
-        };
-    }
+    const data = await response.json();
+    const answer = data.choices[0].text.trim();
+    console.log(data);
 
-    if (!payload || !payload.question) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({ message: 'Payload must contain a "question" property.' }),
-        };
-    }
-
-    const question = payload.question;
-    const apiKey = process.env.CHATGPT_API_KEY;
-
-    try {
-        const response = await fetch(`https://api.openai.com/v1/engines/davinci/completions`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${apiKey}`,
-            },
-            body: JSON.stringify({
-                prompt: question,
-                max_tokens: 50,
-                temperature: 0.5,
-                stop_sequences: ["Yes", "No", "I don't know"]
-            }),
-        });
-
-        if (!response.ok) {
-            throw new Error(`OpenAI API call failed with status: ${response.status}`);
-        }
-
-        const data = await response.json();
-
-        if (!data.choices || data.choices.length === 0) {
-            throw new Error("No choices returned from OpenAI API.");
-        }
-
-        const answer = data.choices[0].text.trim();
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ answer: answer }),
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: `Error processing the request: ${error.message}` }),
-        };
-    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ answer: answer }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: `Error processing the request: ${error.message}`,
+    };
+  }
 };
